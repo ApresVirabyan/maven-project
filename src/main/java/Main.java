54652908e421a8bba59f6457ad015db5738fc0c1
apres <apres1987@mail.ru>
2024-01-26 11:38:02 +0400

    delete not necessary class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
 public class Main {
 
-}
+    public static void main(String[] args) {
+        String str1 = "12222222222222222222222222222222222222";
+        String str2 = "33333333333333333333333333333333333333";
+        System.out.println(findSum(str1, str2));
+    }
 
-class GFG
-{
-    // Function for finding sum of larger numbers
-    static String findSum(String str1, String str2)
-    {
+    static String findSum(String str1, String str2) {
         // Before proceeding further, make sure length
         // of str2 is larger.
-        if (str1.length() > str2.length()){
+        if (str1.length() > str2.length()) {
             String t = str1;
             str1 = str2;
             str2 = t;
@@ -22,33 +22,31 @@ class GFG
         int n1 = str1.length(), n2 = str2.length();
 
         // Reverse both of Strings
-        str1=new StringBuilder(str1).reverse().toString();
-        str2=new StringBuilder(str2).reverse().toString();
+        str1 = new StringBuilder(str1).reverse().toString();
+        str2 = new StringBuilder(str2).reverse().toString();
 
         int carry = 0;
-        for (int i = 0; i < n1; i++)
-        {
+        for (int i = 0; i < n1; i++) {
             // Do school mathematics, compute sum of
             // current digits and carry
-            int sum = ((int)(str1.charAt(i) - '0') +
-                    (int)(str2.charAt(i) - '0') + carry);
-            str += (char)(sum % 10 + '0');
+            int sum = ((int) (str1.charAt(i) - '0') +
+                    (int) (str2.charAt(i) - '0') + carry);
+            str += (char) (sum % 10 + '0');
 
             // Calculate carry for next step
             carry = sum / 10;
         }
 
         // Add remaining digits of larger number
-        for (int i = n1; i < n2; i++)
-        {
-            int sum = ((int)(str2.charAt(i) - '0') + carry);
-            str += (char)(sum % 10 + '0');
+        for (int i = n1; i < n2; i++) {
+            int sum = ((int) (str2.charAt(i) - '0') + carry);
+            str += (char) (sum % 10 + '0');
             carry = sum / 10;
         }
 
         // Add remaining carry
         if (carry > 0)
-            str += (char)(carry + '0');
+            str += (char) (carry + '0');
 
         // reverse resultant String
         str = new StringBuilder(str).reverse().toString();
@@ -56,11 +54,4 @@ class GFG
         return str;
     }
 
-    // Driver code
-    public static void main(String[] args)
-    {
-        String str1 = "12222222222222222222222222222222222222";
-        String str2 = "33333333333333333333333333333333333333";
-        System.out.println(findSum(str1, str2));
-    }
 }
